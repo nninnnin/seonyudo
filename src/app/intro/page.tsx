a7dd45ec3776a7944899eae93f7a380f99b93f70
donggyu <watch@rebel9.co.kr>
2025-02-25 20:07:41 +0900

    빌드에러 고침: DesktopNotice 컴포넌트 dynamic import

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useStore } from "zustand";
 import React, { useEffect } from "react";
 
 import Splash from "@/app/intro/components/Splash";
 import WelcomeDialog from "@/app/intro/components/WelcomeDialog";
-import DesktopNotice from "@/app/intro/components/DesktopNotice";
+
 import { isMobile } from "@/shared/utils/isMobile";
 import { introStore } from "@/app/intro/store/intro";
 import ErrorDialog from "@/app/intro/components/ErrorDialog";
+
+const DesktopNotice = dynamic(
+  () => import("@/app/intro/components/DesktopNotice"),
+  { ssr: false }
+);
 
 const IntroPage = () => {
   if (!isMobile()) {
