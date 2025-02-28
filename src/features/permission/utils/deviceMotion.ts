46374ba493c640cd1cc94e78baec368080ce1dc3
donggyu <watch@rebel9.co.kr>
2025-02-28 14:06:04 +0900

    에러 수정: deviceMotion 권한 요청 로직에 안드로이드 대응 추가

--- src/features/permission/utils/deviceMotion.ts
+++ src/features/permission/utils/deviceMotion.ts
@@ -1,4 +1,33 @@
-export const requestDeviceMotionPermission = async () => {
+const requestDeviceMotionOnSafariIOS = async () => {
   // @ts-ignore
   return await window.DeviceMotionEvent.requestPermission();
+};
+
+const requestDeviceMotionOnAndroidWeb = () => {
+  window.addEventListener("devicemotion", () => {});
+};
+
+export const requestDeviceMotionPermission =
+  async () => {
+    if (isSafariOver13()) {
+      return await requestDeviceMotionOnSafariIOS();
+    } else {
+      requestDeviceMotionOnAndroidWeb();
+
+      return {
+        state: "granted",
+      };
+    }
+  };
+
+const isSafariOver13 = () => {
+  return (
+    window.DeviceOrientationEvent !== undefined &&
+    // @ts-ignore
+    window.DeviceOrientationEvent.requestPermission &&
+    typeof (
+      // @ts-ignore
+      window.DeviceOrientationEvent.requestPermission
+    ) === "function"
+  );
 };
