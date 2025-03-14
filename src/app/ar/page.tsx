9dff8f4dcd4b005b35452c9c53a1f9e5481a041c
donggyu <watch@rebel9.co.kr>
2025-03-14 18:54:00 +0900

    버그 고침: AR 페이지 오버레이 조건부렌더링 리팩토링

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import clsx from "clsx";
-import React, { useEffect } from "react";
-import { useOverlay } from "@toss/use-overlay";
+import React, { useEffect, useState } from "react";
 import {
   redirect,
   useSearchParams,
@@ -13,86 +12,120 @@ import ArFrame from "@/features/ar/components/ArFrame";
 import useLocation from "@/features/location/hooks/useLocation";
 
 const ArPage = () => {
-  const overlay = useOverlay();
   const searchParams = useSearchParams();
 
   const locationName = searchParams?.get("location");
   const { location } = useLocation(locationName ?? "");
 
+  const [hasArContents, setHasArContents] = useState<
+    null | boolean
+  >(null);
+
   useEffect(() => {
-    if (!location?.arContentsUrl) return;
-
-    overlay.open(({ close, isOpen }) => (
-      <>
-        {isOpen && (
-          <Overlay>
-            <div
-              className={clsx(
-                "flex flex-col items-center justify-center",
-                "gap-[10px]",
-                "p-[20px]",
-                "w-[300px] h-[300px]",
-                "bg-white"
-              )}
-            >
-              <h2>AR Guide</h2>
-
-              <p>
-                Lorem ipsum dolor, sit amet consectetur
-                adipisicing elit. Similique quia qui
-                minima impedit necessitatibus. Iure
-                eveniet asperiores dolor placeat
-                voluptates.
-              </p>
-
-              <button
-                className="bg-black text-white p-[10px]"
-                onClick={close}
-              >
-                close
-              </button>
-            </div>
-          </Overlay>
-        )}
-      </>
-    ));
+    if (!location?.arContentsUrl) {
+      setHasArContents(false);
+    } else {
+      setHasArContents(true);
+    }
   }, [location]);
 
-  if (!location || !location.arContentsUrl) {
-    overlay.open(({ isOpen, close }) => (
-      <>
-        {isOpen && (
-          <Overlay>
-            <div
-              className={clsx(
-                "w-[200px] h-[200px] bg-white",
-                "flex flex-col justify-center items-center",
-                "gap-[20px]"
-              )}
-            >
-              <p>AR 컨텐츠가 없습니다.</p>
+  return (
+    <>
+      {hasArContents === false && (
+        <ArPage.NoArContents />
+      )}
 
-              <button
-                onClick={() => {
-                  close();
-                  redirect("/intro");
-                }}
-              >
-                닫기
-              </button>
-            </div>
-          </Overlay>
-        )}
-      </>
-    ));
+      {hasArContents === true && (
+        <ArPage.ArContents
+          arContentsUrl={location!.arContentsUrl}
+        />
+      )}
+    </>
+  );
+};
 
+ArPage.ArContents = ({
+  arContentsUrl,
+}: {
+  arContentsUrl: string;
+}) => {
+  const [showDialog, setShowDialog] = useState(true);
+  const close = () => setShowDialog(false);
+
+  console.log(arContentsUrl);
+
+  return (
+    <>
+      {showDialog && <ArPage.ArGuide close={close} />}
+      <ArFrame src={arContentsUrl} />
+    </>
+  );
+};
+
+ArPage.NoArContents = () => {
+  const [show, setShow] = useState(true);
+
+  if (!show) {
     return <></>;
   }
 
   return (
-    <div>
-      <ArFrame src={location?.arContentsUrl ?? ""} />
-    </div>
+    <Overlay>
+      <div
+        className={clsx(
+          "w-[200px] h-[200px] bg-white",
+          "flex flex-col justify-center items-center",
+          "gap-[20px]"
+        )}
+      >
+        <p>AR 컨텐츠가 없습니다.</p>
+
+        <button
+          onClick={() => {
+            setShow(false);
+            redirect("/intro");
+          }}
+        >
+          닫기
+        </button>
+      </div>
+    </Overlay>
+  );
+};
+
+ArPage.ArGuide = ({
+  close,
+}: {
+  close: () => void;
+}) => {
+  return (
+    <Overlay>
+      <div
+        className={clsx(
+          "flex flex-col items-center justify-center",
+          "gap-[10px]",
+          "p-[20px]",
+          "w-[300px] h-[300px]",
+          "bg-white"
+        )}
+      >
+        <h2>AR Guide</h2>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Similique quia qui minima
+          impedit necessitatibus. Iure eveniet
+          asperiores dolor placeat voluptates.
+        </p>
+
+        <button
+          className="bg-black text-white p-[10px]"
+          onClick={close}
+        >
+          close
+        </button>
+      </div>
+    </Overlay>
   );
 };
 
