200e1575415bc409c42a0593ec0b0831ffe27ca7
donggyu <watch@rebel9.co.kr>
2025-04-10 15:16:57 +0900

    버튼 텍스트 색상 디버깅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,6 @@ export default function Home() {
           );
           router.push("/map");
         }}
-        theme="white"
         iconSource="/icons/pin.svg"
       >
         Open the map
