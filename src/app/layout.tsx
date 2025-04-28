5b550a079b069ea6cbb4c431e6ef9301f5ad6678
donggyu <watch@rebel9.co.kr>
2025-04-28 15:16:43 +0900

    메타데이터 수정: 설명글 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "../styles/globals.css";
 export const metadata: Metadata = {
   title: "Unseeing: 선유동화",
   description:
-    "지난해 7월 '선유도의 과거와 현재를 담은 시민 친화적 작품 구현'이라는 주제로 시민들이 선유도공원을 즐기며 떠오른 이야기와 상상의 아이디어(스토리) 공모에서 출발한 모바일 기반 AR 작품입니다.",
+    "'선유도의 과거와 현재를 담은 시민 친화적 작품 구현'이라는 주제로 시민들이 선유도공원을 즐기며 떠오른 이야기와 상상의 아이디어(스토리) 공모에서 출발한 모바일 기반 AR 작품입니다.",
 };
 
 export default function RootLayout({
